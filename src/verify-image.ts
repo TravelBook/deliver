6274dab5584eec25825b3da2027b4fb161db3044
Kenneth Uy <missingno15@gmail.com>
2025-09-19 18:22:53 +0900

    Add function description and fix indentation

--- src/verify-image.ts
+++ src/verify-image.ts
@@ -1,3 +1,11 @@
+/**
+ * 画像をバリデーションする
+ * 以下の条件を満たす必要がある
+ * - 画像のみにする
+ * - 画像の枚数は指定された枚数、あるいはデフォルトで10枚までにする
+ * - 画像のサイズは指定されたサイズ、あるいはデフォルトで4MB以下にする
+ * @param {Event} event - イベント
+ */
 function verifyImages(event: Event) {
   const target = event.target as HTMLInputElement;
 
@@ -13,28 +21,28 @@ function verifyImages(event: Event) {
   })
 
   if (anyFileNotImage) {
-      alert("画像のみにしてください。");
-      target.value = "";
-      return;
+    alert("画像のみにしてください。");
+    target.value = "";
+    return;
   }
 
   if (target.files.length > imageCountLimit) {
-      alert(`画像の枚数は${imageCountLimit}枚までにしてください。`);
-      target.value = "";
-      return;
+    alert(`画像の枚数は${imageCountLimit}枚までにしてください。`);
+    target.value = "";
+    return;
   }
 
   let invalidImageCount = 0
   for (const img of target.files) {
-      if (img.size > (1_000_000 * megabyteLimit)) {
-          invalidImageCount++;
-      }
+    if (img.size > (1_000_000 * megabyteLimit)) {
+      invalidImageCount++;
+    }
   }
 
   if (invalidImageCount > 0) {
-      alert(`選択された${target.files.length}枚中、${megabyteLimit}MBを超える画像${invalidImageCount}枚がありました。\\n画像サイズは${megabyteLimit}MB以下にしてください。`);
-      target.value = "";
-      return;
+    alert(`選択された${target.files.length}枚中、${megabyteLimit}MBを超える画像${invalidImageCount}枚がありました。\\n画像サイズは${megabyteLimit}MB以下にしてください。`);
+    target.value = "";
+    return;
   }
 }
 
